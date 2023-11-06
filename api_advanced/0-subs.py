b84d04e98d6f4a1aa78b387a02ec7b23ef87410f
Obolo Emmanuel <e.obolo@alustudent.com>
2023-11-06 03:16:55 -0800

    0. How many subs?

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API
+https://www.reddit.com/dev/api/
+and returns the number of subscribers
+(not active users, total subscribers) for a
+given subreddit. If an invalid subreddit is given,
+the function should return 0.
+
+Hint: No authentication is necessary for most features
+of the Reddit API. If you’re getting errors related to
+Too Many Requests, ensure you’re setting a custom User-Agent.
+"""
+import json
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+def number_of_subscribers(subreddit):
+    # create a url
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # create a request object
+    req_object = urllib.request.Request(url, method="GET")
+    # create a custom user agent
+    req_object.add_header('User-Agent', 'OboloScript/1.0')
+    # open the url and send req_object to server and get
+    # a response
+    try:
+        with urllib.request.urlopen(req_object) as res_object:
+            res_json = json.JSONDecoder().decode(res_object.
+                                                 read().decode("utf-8"))
+    except urllib.error.HTTPError:
+        return 0
+    else:
+        return res_json["data"]["subscribers"]
