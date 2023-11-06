efe43afc4015e12c10e883f29dcc1d1f0726d200
Obolo Emmanuel <e.obolo@alustudent.com>
2023-11-06 05:30:25 -0800

    bug fixing in 0-subs and 1-top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -16,6 +16,16 @@ import urllib.request
 
 
 def top_ten(subreddit):
+    """
+    Write a function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given subreddit.
+    Requirements:
+
+    Prototype: def top_ten(subreddit)
+    If not a valid subreddit, print None.
+    NOTE: Invalid subreddits may return a redirect to search
+    results. Ensure that you are not following redirects.
+    """
     # get your url
     url = f"https://www.reddit.com/r/{subreddit}/top.json"
     # create query parameters for pagination and filtering
