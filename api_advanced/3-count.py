5586501ef74039663658a474555fa465acce3e49
OBOLO EMMANUEL OLUWAPELUMI <133635518+eobolo@users.noreply.github.com>
2023-12-04 20:01:24 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,51 +1,44 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API"""
-
-import json
+'''a recursive function that queries the Reddit API,
+ parses the title of all hot articles, and prints a
+ sorted count of given keywords
+'''
 import requests
 
 
-def count_words(subreddit, word_list, after='', hot_list=[]):
-    """Function that queries the Reddit API."""
-    if after == '':
-        hot_list = [0] * len(word_list)
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    request = requests.get(url, params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'My User Agent 1.0'})
-    if request.status_code == 200:
-        data = request.json()
-
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        hot_list[i] += 1
-
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        hot_list[i] += hot_list[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (hot_list[j] > hot_list[i] or
-                            (word_list[i] > word_list[j] and
-                             hot_list[j] == hot_list[i])):
-                        a = hot_list[i]
-                        hot_list[i] = hot_list[j]
-                        hot_list[j] = a
-                        a = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = a
-
-            for i in range(len(word_list)):
-                if (hot_list[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), hot_list[i]))
-        else:
-            count_words(subreddit, word_list, after, hot_list)
+def count_words(subreddit, word_list, fullname="", count=0, hash_table={}):
+    '''fetches all hot posts in a subreddit
+    Return:
+        None - if subreddit is invalid
+    '''
+    if subreddit is None or not isinstance(subreddit, str) or \
+       word_list is None or word_list == []:
+        return
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    params = {'after': fullname, 'limit': 100, 'count': count}
+    headers = {'user-agent': 'Mozilla/5.0 \
+(Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+    info = requests.get(url, headers=headers,
+                        params=params, allow_redirects=False)
+    if info.status_code != 200:
+        return None
+    info_json = info.json()
+    results = info_json.get('data').get('children')
+    new_packet = [post.get('data').get('title') for post in results]
+    for title in new_packet:
+        for word in word_list:
+            word = word.lower()
+            formatted_title = title.lower().split(" ")
+            if word in formatted_title:
+                if (word in hash_table.keys()):
+                    hash_table[word] += formatted_title.count(word)
+                else:
+                    hash_table[word] = formatted_title.count(word)
+    after = info_json.get('data').get('after', None)
+    dist = info_json.get('data').get('dist')
+    count += dist
+    if after:
+        count_words(subreddit, word_list, after, count, hash_table)
+    else:
+        {print('{}: {}'.format(key, value)) for
+         key, value in sorted(hash_table.items(), key=lambda i: (-i[1], i[0]))}
